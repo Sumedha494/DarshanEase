50f994495629de587a78ee981eca2e800426c1e4
Sumedha494 <sumedhasadaphal02@gmail.com>
2026-07-23 09:38:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-/**
- * Merge object b with object a.
- *
- *     var a = { foo: 'bar' }
- *       , b = { bar: 'baz' };
- *
- *     merge(a, b);
- *     // => { foo: 'bar', bar: 'baz' }
- *
- * @param {Object} a
- * @param {Object} b
- * @return {Object}
- * @api public
- */
+const V8ToIstanbul = require('./lib/v8-to-istanbul')
 
-exports = module.exports = function(a, b){
-  if (a && b) {
-    for (var key in b) {
-      a[key] = b[key];
-    }
-  }
-  return a;
-};
+module.exports = function (path, wrapperLength, sources, excludePath) {
+  return new V8ToIstanbul(path, wrapperLength, sources, excludePath)
+}
